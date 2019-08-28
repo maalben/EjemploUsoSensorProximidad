c88c3ec7d685884fda736b866180adf9511e60aa
Mario Alejandro Benitez Orozco <maalben@gmail.com>
2019-08-27 20:31:19 -0500

    Proyecto con la implementación básica del sensor de aproximidad en Android

---
+++ app/src/main/java/com/example/ejemplousosensorproximidad/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.ejemplousosensorproximidad;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textoActivado;
+    private TextView textoDesactivado;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textoActivado = (TextView)findViewById(R.id.lbltitulofinal);
+        textoDesactivado = (TextView)findViewById(R.id.lbltituloinicial);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if(sensor.equals(null)){
+            finish();
+        }
+
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0] < sensor.getMaximumRange()){
+                    System.out.println("Valor sensorEvent -> " + sensorEvent.values[0]);
+                    System.out.println("Rango del sensor -> " + sensor.getMaximumRange());
+                    textoActivado.setVisibility(View.VISIBLE);
+                    textoDesactivado.setVisibility(View.INVISIBLE);
+                    getWindow().getDecorView().setBackgroundColor(Color.rgb(190,204,161));
+                    Toast.makeText(getApplicationContext(), "Sensor activado", Toast.LENGTH_LONG).show();
+                }else{
+                    textoActivado.setVisibility(View.INVISIBLE);
+                    textoDesactivado.setVisibility(View.VISIBLE);
+                    getWindow().getDecorView().setBackgroundColor(Color.rgb(102,102,102));
+                    Toast.makeText(getApplicationContext(), "Sensor inactivado", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        start();
+
+    }
+
+    public void start(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+
+    public void stop(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+}
